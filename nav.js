2b80b09cc23037f58431ce786113f7471dff2e92
Kevin <kevinodeyemi76@gmail.com>
2023-06-28 04:10:25 +0100

    e-commerce website project

---
+++ nav.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.querySelector('.titleImg').classList.add('active');
+  }
+    function menu(){
+        let input  = document.querySelector('#checkInput');
+        input.checked = false;
+    }
